18b9880c86dd5d94d21b2f63c041b336c7aef804
overd <forresst@sheridancollege.ca>
2024-05-13 18:23:03 -0400

    version 1 of PrintList

---
+++ src/printliststudent/PrintListStudent.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package printliststudent;
+
+/**
+ *
+ * @author overd
+ */
+public class PrintListStudent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
